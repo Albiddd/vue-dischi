e7e0581fcfbdc942e581acfea415e437b607d359
Albiddd <alberto.dore93@gmail.com>
2022-09-07 14:35:13 +0200

    init

---
+++ src/components/MainContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <CardComponent/>
+  </div>
+</template>
+
+<script>
+import CardComponent from './CardComponent.vue';
+export default {
+    name: "MainContent",
+    components: { CardComponent },
+    props: {},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
